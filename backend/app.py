45353a89b588799b03f7fddcb02385ae9d3bfc27
David Riobo <blackdragondcr@gmail.com>
2026-04-27 20:39:26 +0200

    add ppost upload_image endpoint base structure

--- backend/app.py
+++ backend/app.py
@@ -8,5 +8,12 @@ CORS(app)
 UPLOAD_DIR = './data/uploads'
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+
+@app.route('/api/upload_image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "no image"}), 400
+    return jsonify({"message": "archivo recibido"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
